c2ee7460d5ecf6dea9215cdb1a421662b8e578b8
Prashant Baisla <prashant.baisla@practo.com>
2016-07-22 14:14:13 +0530

    Add content.js

---
+++ content.js
@@ -0,0 +1,43 @@
+function rateMovies() {
+  var movieAnchorTags = $("div.ml-item a");
+
+  for(var i=0; i< movieAnchorTags.length; i++) {
+    var movieAnchorTag = movieAnchorTags[i].outerHTML;
+    var movieName = getMovieNameFromAnchorTag(movieAnchorTag);
+    var movieYear = getMovieYearFromAnchorTag(movieAnchorTag);
+
+    addMovieRating(movieName, movieYear, i);
+  }
+}
+
+function getMovieNameFromAnchorTag(movieAnchorTag) {
+  var matchResults = movieAnchorTag.match("<b><i>(.*)</i></b>");
+  var movieName = matchResults[1].split('(')[0];
+
+  return movieName;
+}
+
+function getMovieYearFromAnchorTag(movieAnchorTag) {
+  var matchResults = movieAnchorTag.match(/\<b\>\<i\>[\w+\s]* \((\d+)\)\<\/i\>\<\/b\>/);
+  var movieYear = (matchResults? matchResults[1] : null);
+
+  return movieYear;
+}
+
+function addMovieRating(movieName, movieYear, index) {
+  var API = "http://www.omdbapi.com/";
+  var URL = API + "?t=" + encodeURIComponent(movieName) + (movieYear? "&y=" + movieYear : "");
+  $.getJSON(URL, function(response) {
+    if (response.Response === "False") {
+      var rating = "N/A";
+    }
+    else {
+      var rating = response["imdbRating"];
+    }
+
+    $("span.mli-info:eq(" + index + ") h2").after("<h2>Rating: " + rating + "</h2>");
+
+  });
+}
+
+rateMovies();
